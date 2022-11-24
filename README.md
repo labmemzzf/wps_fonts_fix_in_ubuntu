f972ecaec6b138fc4f33e6dd1e143031e023b9be
labmemzzf <labmemzzf@qq.com>
2022-11-24 11:28:59 +0800

    v1.0

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # wps_fonts_fix_in_ubuntu
+
 fixed the fonts missing error for WPS in Ubuntu
+
+run this command:
+
+`bash -e ./fix.sh`
--- fix.sh
+++ fix.sh
@@ -1,2 +1,15 @@
 #!/bin/bash
 echo "start to fixing the fonts missing error for WPS in Ubuntu"
+# uncompress and copy the font files to /usr/share/fonts
+unzip ./wps-fonts.zip
+sudo cp ./wps-fonts/* /usr/share/fonts
+
+# make index
+cd /usr/share/fonts
+sudo mkfontscale
+sudo mkfontdir
+
+# update cache
+sudo fc-cache
+
+echo "fixed"
